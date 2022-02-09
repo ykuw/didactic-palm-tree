7436f385084fdcf29f4e0862562680e79978bb9a
ldcu <98633536+ykuw@users.noreply.github.com>
2022-02-09 13:45:40 +0200

    2836fec996430ad323bd4c7996053a37

--- 8x8_delete_vcc_numbers_vcc_api.py
+++ 8x8_delete_vcc_numbers_vcc_api.py
@@ -1,7 +1,9 @@
+import datetime
 import requests
+from requests import adapters
 import sys
-import time
-from config import credentials, url
+import ssl
+from config import credentials, url, site, cluster, customer_id
 
 
 def authentication():  # Getting the token.
@@ -11,38 +13,56 @@ def authentication():  # Getting the token.
 	return sso.json()["access_token"]
 
 
-bearer = {'Authorization': 'Bearer ' + authentication()}  # Bearer token for the API requests.
+class TLSAdapter(adapters.HTTPAdapter):  # This solves a SSL issue
+	# That issue is 'ssl.SSLError: [SSL: WRONG_SIGNATURE_TYPE] wrong signature type (_ssl.c:997)'
+	def init_poolmanager(self, *args, **kwargs):
+		ctx = ssl.create_default_context()
+		ctx.set_ciphers('DEFAULT@SECLEVEL=1')
+		kwargs['ssl_context'] = ctx
+		return super(TLSAdapter, self).init_poolmanager(*args, **kwargs)
 
-tenant = input("Enter the tenant name: ")  # Getting tenant name.
-site = input("Enter the site: ").upper()  # Getting the site for the tenant. Making the input uppercase.
-cluster = input("Enter the cluster: ").upper()  # Getting the cluster for the tenant. Making the input uppercase.
-get_numbers = input("Enter the telephone numbers to be deleted, comma separated: ")  # Getting tel. num.
 
-numbers = get_numbers.replace(" ", "").split(",")  # Removing any space character and creating a list with the input.
+session = requests.session()
+session.mount('https://', TLSAdapter())
 
-# If no number is entered, exiting the script.
-if len(numbers) > 0:
-	confirmation = input(f"Do you want to remove all telephone numbers pasted for {tenant}? Enter Y or N: ")
-else:
-	print("No numbers provided, exiting the script.")
+bearer = {'Authorization': 'Bearer ' + authentication(), 'Content-Type': 'application/xml'}
+
+log = open("apis.log", "a")  # Logging all API requests.
+
+file = open("numbers.txt", "r")  # Licenses to be removed.
+
+if not log.writable():  # Checking if we can write the log file.
+	print("Cannot write the log file.")
 	sys.exit()
+
+if not file.readable():  # Checking if we can read the text file.
+	print("Cannot read the text file.")
+	sys.exit()
+
+confirmation = input(f"Do you want to remove all numbers the text file for {customer_id}? Enter Y or N: ")
 
 count = 0  # Used for counting the tel. num. removed.
 
 if confirmation in ["Y", "y"]:
-	for number in numbers:
-		xml = f'<request><user id="pma" agent="Account Manager"></user><phone clidblk="enabled" calling-name="8x8, ' \
-			  f'Inc." site="{site}" cluster="{cluster}"></phone></request>'
-		delete_number = requests.post(
-			f"https://vcc-provapi-prod.{url}/tenant/{tenant}/phone/delete/{number}", headers=bearer, data=xml)
-		if delete_number.status_code == 200:
-			print(f"Successfully removed {number} from {tenant}!")
+	for row in file.readlines():
+		a_number = row.strip("\n")
+		xml = f'<request><user id="pma" agent="Account Manager"></user><phone clidblk="enabled" ' \
+			  f'calling-name="8x8, Inc." site="{site.upper()}" cluster="{cluster.upper()}"></phone></request>'
+		delete_number = session.post(f"https://vcc-provapi-prod.{url}/tenant/{customer_id}/phone/delete/{a_number}",
+									 headers=bearer, data=xml)
+		if delete_number.ok:
+			print(f"Successfully removed {a_number} from {customer_id}!")
+			log.write(f"[{datetime.datetime.now()}]\tSuccessfully removed {a_number} from "
+					  f"{customer_id}!\t{delete_number.request.url}\t{delete_number.status_code}\t{delete_number.request.body}\n")
 			count += 1
-			time.sleep(5)
 		else:
 			print(f"Unable to execute the API request. Response code is {delete_number.status_code}.")
+			log.write(f"[{datetime.datetime.now()}]\tUnable to execute the API request. Response code "
+					  f"is {delete_number.status_code}\t{delete_number.request.url}\t{delete_number.request.body}\n")
 			sys.exit()
-	print(f"{count} telephone numbers deleted for {tenant} from VCC CM.")
+	print(f"{count} telephone numbers removed for {customer_id} from VCC CM.")
+	log.write(f"[{datetime.datetime.now()}]\t{count} telephone numbers deleted for {customer_id} from VCC CM.\n")
 else:
 	print("Operation aborted.")
+	log.write(f"[{datetime.datetime.now()}]\tOperation aborted.\n")
 	sys.exit()
--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 credentials = ""  # API credentials.
 customer_id = ""  # Customer ID.
+site = ""  # Tenant site.
+cluster = ""  # Tenant cluster.
 url = ""  # Website.
 start_date = ""  # Start date.
 end_date = ""  # End date.
