726be3cf29dae03175aeb3cfd0e813a8cc101975
ykuw <lentils-aspic0p@icloud.com>
2022-05-17 01:57:11 +0300

    2836fec996430ad323bd4c7996053a37

--- generate_random_pin_code.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-numbers_of_characters = int(input("Enter the number of characters for the pin: "))
-
-id = "".join(random.SystemRandom().choice(string.digits) for _ in range(numbers_of_characters))
-
-print(id)
--- run_sql_query_based_on_txt_output_as_csv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-filename="/home/scripts/db/cli.txt"
-
-cat $filename | while read line || [ -n "$line" ]; do echo "SELECT c.DDI, c.CLI, c.datetime AS Timestamp, c.duration AS Duration FROM calls c JOIN queues q ON q.uid = c.qid AND q.grouping = AND q.type = 'outbound' WHERE c.datetime BETWEEN '2019-01-01' AND '2019-07-23' AND c.call_outcome NOT IN ('101', '102') AND c.CLI = ${line}" | mysql -h -u -p db | sed 's/\t/,/g' > output/${line}.csv; done
-
-exit 0
